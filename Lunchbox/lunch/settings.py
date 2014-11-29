8d6e458bc2ac653829e72bdda4e639f1e4e65491
jableader <jacobdunk@gmail.com>
2014-11-30 07:56:13 +1100

    actually work

--- Lunchbox/lunch/settings.py
+++ Lunchbox/lunch/settings.py
@@ -87,9 +87,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "lunchapp/static")
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'lunchapp/templates'),
 )
 
-MEDIA_ROOT = 'lunchapp/static/img/uploaded'
+MEDIA_ROOT = os.path.join(STATIC_ROOT, 'img/uploaded')
--- Lunchbox/lunch/wsgi.py
+++ Lunchbox/lunch/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 if os.name != 'nt':
     path = '/home/jableader/lunch/Lunchbox'
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lunch.settings")
 from django.core.wsgi import get_wsgi_application
