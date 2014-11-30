975e5f171b31f7a9f7b21416ecd90f8b30ddf965
jableader <jacobdunk@gmail.com>
2014-11-30 11:19:07 +1100

    Im so fancy

--- Lunchbox/lunchapp/admin.py
+++ Lunchbox/lunchapp/admin.py
@@ -4,10 +4,6 @@ from model_admins import ItemAdmin, GroupAdmin, MenuAdmin, ParentAdmin
 import models
 
 # Register your models here.
-admin.site.register(models.Canteen)
-admin.site.register(models.LunchLady)
-admin.site.register(models.Parent, ParentAdmin)
-admin.site.register(models.Kid)
 admin.site.register(models.Menu, MenuAdmin)
 admin.site.register(models.Group, GroupAdmin)
 admin.site.register(models.Item, ItemAdmin)
